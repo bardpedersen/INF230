e692c999e27282a5e2d202149987a6acf595d4eb
Bård Pedersen <bard.tollef.pedersen@nmbu.no>
2023-09-27 13:55:26 +0200

    better

--- Assignment_1/Assignment1.sql
+++ Assignment_1/Assignment1.sql
@@ -1,53 +1,53 @@
 #Group number: G05_Tues_10
 #Group members: Peder Ørmen Bukaasen, Mohammed Idris Omar, Bård Tollef Pedersen.
 
-# Task 1
+--@block Task 1
 SELECT * FROM Categories;
-
-# Task 2
+   
+--@block Task 2
 SELECT ProductName, QuantityPerUnit, UnitPrice FROM Products;
 
-# Task 3
+--@block Task 3
 SELECT DISTINCT ContactTitle FROM customers;
 
-# Task 4
+--@block Task 4
 SELECT DISTINCT Title FROM employees;
 
-# Task 5
+--@block Task 5
 SELECT * FROM Orders WHERE OrderDate = '1996-05-17';
 
-# Task 6
+--@block Task 6
 SELECT * FROM Orders WHERE OrderDate >= '1996-05-17'; 
 
-# Task 7
+--@block Task 7
 SELECT * FROM Products WHERE UnitPrice > '40';
 
-# Task 8
+--@block Task 8
 SELECT * FROM Products WHERE UnitPrice BETWEEN 30 AND 40;
 
-# Task 9
+--@block Task 9
 SELECT OrderID, ContactName FROM Orders, Customers;
 
- # Task 10
+--@block Task 10
 SELECT Customers.ContactName, Customers.Address, Customers.City, Orders.OrderDate FROM Orders, Customers WHERE Orders.OrderDate = '1995-05-01' AND Customers.CustomerID = Orders.CustomerID;
 
- # Task 11    
+--@block Task 11    
 SELECT * FROM Customers WHERE City = 'London' OR City = 'Buenos Aires';
 
-# Task 12 
+--@block Task 12 
 SELECT * FROM Customers WHERE Country = 'USA' AND NOT City = 'Portland';
 
-# Task 13
+--@block Task 13
 SELECT * FROM orderdetails WHERE ProductID IN ( 
-SELECT ProductID
-FROM products
-WHERE ProductName = 'Tofu'
+	SELECT ProductID
+	FROM products
+	WHERE ProductName = 'Tofu'
 );
 
-# Task 14
+--@block Task 14
 SELECT employees.FirstName, employees.LastName FROM orders, employees WHERE orders.OrderID = 10369 AND employees.EmployeeID = orders.EmployeeID; 
 
-# Task 15
+--@block Task 15
 SELECT FirstName, LastName FROM Employees WHERE EmployeeID = (
     SELECT EmployeeID
     FROM orders
